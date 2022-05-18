1e1c845845e66f0def0fc9df5a8d39e46bf0b2d1
Jhozua Manguera Diaz <diaz.jhozua@gmail.com>
2022-05-18 11:24:58 +0800

    Add project files.

---
+++ MahikapeWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using MahikapeWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MahikapeWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category>? Categories { get; set; }
+
+    }
+}
---
+++ MahikapeWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MahikapeWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
